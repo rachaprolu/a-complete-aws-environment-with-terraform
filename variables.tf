19cc766cddbbe0a942008b1dcc5313d7d426f658
Raju Rachaprolu <31129348+rachaprolu@users.noreply.github.com>
2021-05-13 11:41:20 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,18 +7,18 @@ variable "AmiLinux" {
     us-east-1 = "ami-b73b63a0" # Virginia
     us-west-2 = "ami-5ec1673e" # Oregon
     eu-west-1 = "ami-9398d3e0" # Ireland
-    us-east-2 = "ami-ea87a78f" # Ohio
+    us-east-2 = "ami-077e31c4939f6a2f3" # Ohio
   }
   description = "I add only 3 regions (Virginia, Oregon, Ireland) to show the map feature but you can add all the regions that you need"
 }
 /*
 variable "aws_access_key" {
-  default = "xxxxx"
+  default = "AKIA43KVDCQAWPATKIK2"
   description = "the user aws access key"
 }
 
 variable "aws_secret_key" {
-  default = "xxxx"
+  default = "a2y/Fp8zFROTl515AGrR3EE24LFdW6ZitsBmgYp5"
   description = "the user aws secret key"
 }
 */
@@ -40,7 +40,7 @@ variable "Subnet-Private-AzA-CIDR" {
   description = "the cidr of the subnet"
 }
 variable "key_name" {
-  default = "demotmp"
+  default = "last-key"
   description = "the ssh key to use in the EC2 machines"
 }
 variable "DnsZoneName" {
